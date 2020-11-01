40083e87f33ee7a3143598f7d7aae8ecff316f33
nguyenthingan20191985 <72119054+nguyenthingan20191985@users.noreply.github.com>
2020-11-01 07:31:17 +0700

    Add files via upload

---
+++ web viện.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     var x1=true;
+//     var x2 = document.getElementsByClassName('menu');
+//     // console.log(x2);
+//     window.addEventListener("scroll", function(){
+//         //  console.log(window.pageYOffset);
+//         if(x1==true){
+//             if(window.pageYOffset>200){
+//                 x2[0].classList.add('dungyen');
+//                 x1=false;
+//             }
+//         }
+//     })
+
+// }, false)
